4d16f3b5d1eed953c19cbc7dbadb2f0e6a590617
Isaac Ayomi <Okunlolaa3@outlook.com>
2025-07-03 16:12:05 +0100

    fix

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,10 +1,10 @@
-import { useLayoutEffect, useRef, useEffect } from "react";
+import { useLayoutEffect, useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const PageHeading = ({ prefix, keyword, className }) => {
+const PageHeading = ({ prefix, keyword }) => {
   const headingRef = useRef();
 
   useLayoutEffect(() => {
@@ -31,21 +31,24 @@ const PageHeading = ({ prefix, keyword, className }) => {
   }, []);
 
   useEffect(() => {
-    ScrollTrigger.refresh(); // Ensures correct positioning in production
+    ScrollTrigger.refresh();
   }, []);
 
   return (
-    <h3
-      ref={headingRef}
-      className={`${className} text-very-dark-blue text-[3rem] font-bold leading-[3.25rem] tracking-[-0.10713rem]
-       max-w-[19.4375rem] mx-auto mt-[1.25rem] mb-[1.38rem] text-left
-       md:max-w-[35.75rem] md:text-[4.5rem] md:leading-[4rem] md:tracking-[-0.125rem]
-       md:font-spartan md:mt-[-4.5rem] md:pb-[2.69rem]
-       lg:max-w-none lg:mx-0 lg:mt-0`}
-    >
-      <span className="md:block">{prefix}</span>
-      <span className="md:block">{keyword}</span>
-    </h3>
+    <div>
+      <h3
+        ref={headingRef}
+        className="text-very-dark-blue text-[3rem] non-italic font-bold leading-[3.25rem] tracking-[-0.10713rem]
+        max-w-[19.4375rem] w-full mx-auto mt-[1.25rem] mb-[1.38rem] text-left
+        md:max-w-[35.75rem] md:w-full md:mx-auto md:text-left md:text-[4.5rem]
+        md:leading-[4rem] md:tracking-[-0.125rem] md:font-spartan md:font-bold
+        md:text-very-dark-blue md:mt-[-4.5rem] md:pb-[2.69rem] md:pl-0
+        lg:pt-[2.7rem] lg:pb-[2.69rem]"
+      >
+        <span className="md:block">{prefix}</span>
+        <span className="md:block">{keyword}</span>
+      </h3>
+    </div>
   );
 };
 
